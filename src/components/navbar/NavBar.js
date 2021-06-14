98835655c01eddbee316f55fde60dda4a970dccb
Alejandro Maure <alejandrodanielmaure@gmail.com>
2021-06-14 01:28:55 -0300

    Desafio Menu E-commerce subido

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../../index.css';
+import logo from '../../img/logo.jpeg';
+import search from '../../svg/search.svg'
+import cart from '../../svg/shopping-cart.svg'
+import user from '../../svg/user.svg'
+
+export const NavBar = () => {
+    return(
+        <div>
+            <nav className="bg-black">
+                <div className="">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0 ml-5">
+                                <img className="h-20 w-20" src={logo} alt="" />
+                            </div>
+                            <div className="ml-20 space-x-10 flex">
+                                <button><span className="text-white">INICIO</span></button>
+                                <button><span className="text-white">LENCERIA</span></button>
+                                <button><span className="text-white">ARNESES</span></button>
+                                <button><span className="text-white">ROPA</span></button>
+                                <button><span className="text-white">ACCESORIOS</span></button>
+                            </div>
+                            <div className="ml-4 flex items-center space-x-7 absolute right-20">
+                                <button><img className="h-6 w-6" src={search} alt="" /></button>
+                                <button><img className="h-6 w-6" src={user} alt="" /></button>
+                                <button><img className="h-6 w-6" src={cart} alt="" /></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+import '../../index.css'
+import NavBar from '../../components/navbar/NavBar'
+
+function Home() {
+  return (
+    <div className="Home">
+      <NavBar />
+    </div>
+  );
+}
+
+export default Home;
